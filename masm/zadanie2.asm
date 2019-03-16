a6c9f3fd6919baa292769f78ee8f3a5fd3084018
Piotrek Kotara <piotrekkotara@gmail.com>
2019-03-16 22:15:17 +0100

    started zad2

--- masm/zadanie2.asm
+++ masm/zadanie2.asm
@@ -4,13 +4,32 @@ data segment
     ; add your data here!
     pkey db "press any key...$"   
     handle dw ?
-    
-    
+        
+    ;BITMAPFILEHEADER ma 14 bajtow
+    bfType	dw	?
+    bfSize	dd	?
+    bfReserved1	dw	?
+    bfReserved2	dw	?
+    bfOffBits	dd	?
+    ;BITMAPINFOHEADER ma 40 bajtow
+    biSize	dd	?
+    biWidth	dd	?
+    biHeight	dd	?
+    biPlanes	dw	?
+    biBitCount	dw	?
+    biCompression	dd	?
+    biSizeImage	dd	?
+    biXPelsPerMeter	dd	?
+    biYPelsPerMeter	dd	?
+    biClrUsed	dd	?
+    biClrImportant	dd	?
+    ;Paleta kolorow
+    palette	dd	256 dup (?)
 data ends  
 
 image segment
       
-    buf db   65535 dup (0)
+    pixelArray db   64000 dup (0) ; tablica pikseli
 image ends
 
 stack segment stack
@@ -43,6 +62,10 @@ start:
     ; wait for any key....    
     mov ah, 1
     int 21h        
+
+    mov al, 13h
+	mov ah, 0
+	int 10h     ; set graphics video mode. 
     
     
     mov al, 0 ; read mode
@@ -52,9 +75,7 @@ start:
 	
 	jc  endd  ;if error on opening file end program
     
-    mov dx, seg dane
-    mov ds, dx ; ustaw segment danych, juz nie potrzebujemy nazwy pliku
-                  
+         
     mov word ptr ds:handle ,ax ;file handle   
                                        
     
@@ -62,35 +83,68 @@ start:
     
  
     mov bx, word ptr ds:handle; wpisz handle do pliku
-    mov cx, 0ffffh; ilosc bajtow                        
-    mov dx, seg buf
+    mov cx, 1078; ilosc bajtow do zczytania pliku od poczatku do palety kolorow                        
+    mov dx, seg bfType
     mov ds, dx
-    mov dx, offset buf  
+    mov dx, offset bfType  
+    mov ah, 3fh ; czytaj z pliku
+    int 21h
+
+
+    mov dx, 0A000h ; wskaz na pamiec vga
+	mov es, dx
+
+    mov bx, word ptr ds:handle; wpisz handle do pliku
+    mov cx, 0FA00h; wybierz 320x200 bajtow i zapisz je do odpowiedniego bufora
+    mov dx, 0A000h
+    mov ds, dx
+    mov dx, offset 0  
     mov ah, 3fh ; czytaj z pliku
     int 21h
     
+    ; mov dx, seg pixelArray
+    ; mov ds, dx
+    ; mov dx, offset pixelArray      
+
+    ; mov bx, seg palette
+    ; mov di, offset palette
+
+    ; mov ax, 0
+    ; pixelLoop:
+    
+    ; mov dword ptr, 
+
+
+    ; inc ax
+    ; cmp ax, 0‭FA00‬h
+    ; jl pixelLoop
+
+    ; wait for any key....    
+   ; mov ah, 1
+    ;int 21h   
     
     mov bx, word ptr ds:handle; wpisz handle do pliku
     mov ah, 3eh
     int 21h ;zamknij plik
     
-    mov al, 13h
-	mov ah, 0
-	int 10h     ; set graphics video mode. 
+    endd:  
+    mov ah, 1
+    int 21h 
+
+    mov ax, 4c00h ; exit to operating system.
+    int 21h    
+    errendd:
 	mov al, 1100b
 	mov cx, 10
 	mov dx, 20
 	mov ah, 0ch
 	int 10h     ; set pixel.
-	 
-    ; wait for any key....    
-    mov ah, 1
-    int 21h   
+    ;  lea dx, ds:[82h] 
     
-    endd:
-        
-    mov ax, 4c00h ; exit to operating system.
-    int 21h    
+    ; mov ah, 9
+    int 21h
+    jmp endd
+
 code ends
 
 end start ; set entry point and stop the assembler.
