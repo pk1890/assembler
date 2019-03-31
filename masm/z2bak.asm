747e8db5fd3f82df1b4b63703f0e9711c66d14ba
pk1890 <piotrekkotara@gmail.com>
2019-03-31 16:19:12 +0200

    added 24 bit format BMP support

--- masm/z2bak.asm
+++ masm/z2bak.asm
@@ -1,10 +1,10 @@
 
 data segment
 
-    errormsg db "Wrong input file format $"   
+    pkey db "press any key...$"   
     currx dw 0000h 
     curry dw 0000h
-    zoom dw 0001h
+    zoom db 00h
     effectflags db 00000000b
     handle dw ?
         
@@ -32,13 +32,8 @@ data ends
 
 image segment
       
-    pixelArray db   0fffch dup (0) ; tablica pikseli
-    red24Val db ?
-    green24Val db ?
-    blue24Val db ?
+    pixelArray db   0ffffh dup (0) ; tablica pikseli
 image ends
-
-    
 
 stack segment stack
         dw   128  dup(0)
@@ -91,39 +86,13 @@ start:
 
     MAINLOOP:
 
-    call loadPalette
-    
-
     mov cx, 200
-    mov di, cx
-    push dx
     drawRowLoop:
-        call loadRow
-        push cx
-        mov cx, di
-        mov dx, seg zoom
-        mov ds, dx
-        mov dx, word ptr ds:zoom 
-
-
-        zoomLoop:
         call drawRow
-        dec di
-        dec cx
-        dec dx
-        cmp dx, 0
-        jg zoomLoop
-
         ; call drawRow
-        ; dec di
+    loop drawRowLoop
 
-        pop cx
-        dec cx
-        cmp di, 0
-        jg drawRowLoop
-
-    ;  loop drawRowLoop
-pop dx
+    call loadPalette
 
    
 
@@ -145,10 +114,6 @@ pop dx
     je GoLeft
     cmp ah, 04Dh ;male a strzalka w dol
     je GoRight
-    cmp ah, 04Eh ;male a strzalka w dol
-    je zoomUp 
-    cmp ah, 04Ah ;male a strzalka w dol
-    je zoomDown
     cmp al, '1'
     je switchRed
     cmp al, '2'
@@ -162,31 +127,6 @@ pop dx
     
     jmp MAINLOOP
 
-
-    zoomUp:
-    mov dx, seg zoom
-    mov ds, dx
-    mov dx, word ptr ds:zoom
-    cmp dx, 4
-    je MAINLOOP
-    shl dx, 1
-    mov word ptr ds:zoom, dx
-
-
-    jmp MAINLOOP
-
-    zoomDown:
-    mov dx, seg zoom
-    mov ds, dx
-    mov dx, word ptr ds:zoom
-    cmp dx, 1
-    je MAINLOOP
-    shr dx, 1
-    mov word ptr ds:zoom, dx
-
-
-    jmp MAINLOOP
-
     GoDown:
         mov dx, seg curry
         mov ds, dx 
@@ -195,44 +135,32 @@ pop dx
         mov word ptr ds:curry, dx
     jmp MAINLOOP
    GoUp:
-        mov dx, seg curry
-        mov ds, dx 
         mov dx, word ptr ds:curry
         sub dx, 2
         mov word ptr ds:curry, dx
     jmp MAINLOOP
 
    GoLeft:
-        mov dx, seg curry
-        mov ds, dx 
         mov dx, word ptr ds:currx
         sub dx, 2
         mov word ptr ds:currx, dx
     jmp MAINLOOP
         
    GoRight:
-        mov dx, seg curry
-        mov ds, dx 
         mov dx, word ptr ds:currx
         add dx, 2
         mov word ptr ds:currx, dx
     jmp MAINLOOP
         
     switchRed:
-        mov dx, seg curry
-        mov ds, dx 
         xor byte ptr ds:effectflags[0], 00000001b
     jmp MAINLOOP
 
     switchGreen:
-        mov dx, seg curry
-        mov ds, dx 
         xor byte ptr ds:effectflags[0], 00000010b
     jmp MAINLOOP
 
     switchBlue:
-        mov dx, seg curry
-        mov ds, dx 
         xor byte ptr ds:effectflags[0], 00000100b
     jmp MAINLOOP
 
@@ -269,35 +197,33 @@ pop dx
     endd:  
  
 
-    mov ah, 0
-    mov al, 03h
-    int 10h
 
  ;   mov ah, 1
   ;  int 21h 
+
+    mov ah, 0
+    mov al, 03h
+    int 10h
 
 
     mov ax, 4c00h ; zakoncz program
     int 21h    
     errendd:
-
-    mov ah, 0
-    mov al, 03h
-    int 10h
-
-     
-    mov dx, offset errormsg 
-    mov ax, seg errormsg
-    mov ds, ax
-    mov ah, 9 ;wypisz stringa z from ds:dx
-    int 21h  
     
-    mov ax, 4c00h ; zakoncz program
-    int 21h    
+    mov dx, 0A000h ; wskaz na pamiec vga
+	mov es, dx
 
-    loadRow: ; wystietl 1 linie obrazka (nr linii w cx)
+    mov bx, 500
+    llp:
+    mov byte ptr es:[bx], 13
+    dec bx
+    jne llp
+    jmp endd
+
+    drawRow: ; wystietl 1 linie obrazka (nr linii w cx)
         push cx ;zeby zachowac ten z petli
         dec cx
+
         mov dx, seg currx 
         mov ds, dx
 
@@ -320,7 +246,6 @@ pop dx
         mov ax, word ptr ds:curry
         sub cx, ax ; cx = height - cx - curry
 
-
         mov ax, cx
         mov cx, word ptr ds:biWidth
 
@@ -330,7 +255,7 @@ pop dx
         push dx
         
         mov ax, cx
-        mov bx, word ptr ds:biBitCount ;;liczba bitow na piksel
+        mov bx, 8 ;;liczba bitow na piksel
         mul bx 
         
         add ax, 31
@@ -359,8 +284,9 @@ pop dx
         mov ah, 42h
         int 21h ; przejdz na poczatek szukanej linii
 
-        cmp byte ptr ds:biBitCount, 24
-        je readLine24
+        mov dx, seg handle
+        mov ds, dx
+        mov bx, word ptr ds:handle; wpisz handle do pliku
 
         mov cx, word ptr ds:biWidth
         mov dx, seg pixelArray
@@ -369,134 +295,19 @@ pop dx
         mov ah, 3fh ; czytaj z pliku linie
         int 21h
 
-        pop ax
-        pop cx 
-        ret
-
-        readLine24:
-        push di
-        mov di, 0
-
-        preparePixel:        
-
-        mov cx, 3 ; sczytaj 3 bajty 
-        mov dx, seg red24Val
-        mov ds, dx
-        mov dx, offset red24Val  
-        mov ah, 3fh ; czytaj z pliku 3 bajty (info o jednym pikselu)
-        int 21h
-
-        ; mov dx, seg biWidth
-        ; mov ds, dx 
-        ; mov cx, word ptr ds:biWidth
-        ; mov dx, seg pixelArray
-        ; mov ds, dx
-        ; mov dx, offset pixelArray  
-        ; mov ah, 3fh ; czytaj z pliku linie
-        ; int 21h
-
-
-
-        mov ax, 0
-        mov ah, byte ptr ds:red24Val
-        and ah, 11100000b ; wytnij 3 najstarsze bajty
-
-        add al, ah ; w al - RRR00000
-
-        mov ah, byte ptr ds:green24Val
-        and ah, 11100000b ; wytnij 3 najstarsze bajty
-        mov cl, 3
-        shr ah, cl
-        
-        add al, ah ; w al - RRRGGG00 
-
-        mov ah, byte ptr ds:blue24Val
-        and ah, 11000000b ; wytnij 2 najstarsze bajty
-        mov cl, 6
-        shr ah, cl
-        
-        add al, ah ; w al - RRRGGGBB
-        ; mov al, byte ptr ds:red24Val
-        mov dx, seg pixelArray
-        mov ds, dx
-        ; neg al
-        mov byte ptr ds:pixelArray[di], al 
-
-        inc di
-        mov dx, seg biWidth
-        mov ds, dx
-        cmp di, word ptr ds:biWidth
-        jl preparePixel
-
-        pop di
-        pop ax
-        pop cx 
-        ret
-
-        drawRow:
-        push ax
-        push bx
-        push cx 
-        push dx
-
-        dec cx
-
         mov dx, seg currx
         mov ds, dx
         
-        ; pop ax ; nr linii ktora chcemy wyswietlic
-        mov ax, cx
+        pop ax ; nr linii ktora chcemy wyswietlic
         mov bx, 320
         mul bx
         mov cx, 320
-        mov dx, cx
-        push di
-        ;mov di, 8
         LineLoop:
-        mov di, seg zoom
-        mov ds, di
-        mov di, word ptr ds:zoom
-        pixZoomLoop:
         call setPixelLine
-        dec dx
-        dec di
-        cmp di, 0
-        jg pixZoomLoop
-        
         loop LineLoop
-        pop di
-;;;;;;;;;;;;;;;;;
 
-    ; drawRowLoop
-    ;     call loadRow
-    ;     push cx
-    ;     mov cx, di
-    ;     mov dx, seg zoom
-    ;     mov ds, dx
-    ;     mov dx, word ptr ds:zoom 
 
-    ;     zoomLoop:
-    ;     call drawRow
-    ;     dec di
-    ;     dec cx
-    ;     dec dx
-    ;     cmp dx, 0
-    ;     jg zoomLoop
-
-    ;     ; call drawRow
-    ;     ; dec di
-
-    ;     pop cx
-    ;     dec cx
-    ;     cmp di, 0
-    ;     jg drawRowLoop
-;;;;;;;;;;;;;;;;
-
-        pop dx
         pop cx
-        pop bx
-        pop ax
-
         ret
 
     ; setPixelLine: ; wersja z wyswietlaniem piksela na aktualnej linii (w ax = y, w cx x)
@@ -533,12 +344,8 @@ pop dx
     ;     pop cx 
     ;     pop ax
     ;     ret
-
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-    setPixelLine: ; wersja z wyswietlaniem piksela na aktualnej linii (w ax = 320*y, w cx x) x w pamieci obrazu w dx
+    setPixelLine: ; wersja z wyswietlaniem piksela na aktualnej linii (w ax = 320*y, w cx x)
         push ax
-        push dx
         push cx
 
         dec cx ; poprawka na to ze w petli cx jesrt od 320 do 1 a potzreba od 319 do 0
@@ -568,16 +375,9 @@ pop dx
         mov al, byte ptr ds:[bx] 
        ; mov al, 10
         mov bx, cx
-        pop cx
-        sub bx, cx
-        pop dx
-        add bx, dx
- ;       sub bx, 320
-    ;    sub bx, 320
-
         mov byte ptr es:[bx], al
         
-        ;  pop cx
+        pop cx
         pop ax
         ret
 
@@ -612,13 +412,6 @@ pop dx
         ret
     
     loadPalette:
-        cmp ds:biBitCount, 8 
-        je loadPalette8
-        cmp ds:biBitCount, 24
-        je loadPalette24
-        jmp errendd
-
-        loadPalette8:
         push ax
         push bx
         push cx 
@@ -633,6 +426,11 @@ pop dx
         mov cx, 256
         mov di, offset ds:palette
         loadloop:
+        ; mov di, 256*4
+        ; sub di, cx
+        ; mov dx, offset palette
+        ; add di, dx
+        ;mov ax, 0ffffh
         push cx
         mov cl, 02h
         
@@ -685,84 +483,6 @@ pop dx
         pop  ax
         ret
 
-        loadPalette24: ; konwencja: RRRGGGBB
-        push ax
-        push bx
-        push cx 
-        push dx
-
-        mov dx, 3C8h
-        mov al, 0
-        out dx, al
-        push bx
-        mov cx, 0000h
-        loadloop24:
-        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;BLUE;;;;;;;;;;;;;;;;;;;
-        mov al, cl
-        and al, 00000011b ; wydobadz dwa najmlodsze bity 
-        mov dh, 4
-        xchg dh, cl
-        shl al, cl ; przesun je tak by najwieksza wartosc byla rowna 63
-        xchg dh, cl
-
-        mov bl, byte ptr ds:effectflags[0]
-        and bl, 00000100b
-        je skipblueeff24
-
-        mov al, 00h
-        skipblueeff24:
-
-        mov dx, 3C9h
-        out dx, al
-
-        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;GREEN;;;;;;;;;;;;;;;;;;;
-        mov al, cl
-        and al, 00011100b ; wydobadz trzy kolejne bity 
-        mov dh, 01h
-        xchg dh, cl
-        shl al, cl ; przesun je tak by najwieksza wartosc byla rowna 63
-        xchg dh, cl
-        
-        mov bl, byte ptr ds:effectflags[0]
-        and bl, 00000010b
-        je skipgreeneff24
-
-        mov al, 00h
-        skipgreeneff24:
-
-        mov dx, 3C9h
-        out dx, al
-        
-        
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;RED
-        mov al, cl
-        and al, 11100000b ; wydobadz dwa trzy najstarsze bity
-        mov dh, 02h
-        xchg dh, cl
-        shr al, cl ; przesun je tak by najwieksza wartosc byla rowna 63
-        xchg dh, cl
-
-        mov bl, byte ptr ds:effectflags[0]
-        and bl, 00000001b
-        je skipredeff24
-        mov al, 00h
-        
-        skipredeff24:
-        mov dx, 3C9h
-        out dx, al
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-         inc cx
-         cmp cx, 00ffh
-         jle loadloop24
-        ;loop loadloop24
-        pop bx
-
-        pop  dx
-        pop  cx 
-        pop  bx
-        pop  ax
-        ret
 code ends
 
 end start
